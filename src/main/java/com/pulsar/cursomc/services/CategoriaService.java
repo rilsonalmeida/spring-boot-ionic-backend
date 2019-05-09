13927a52d8a2f709aaf6d37035ae1100ad4c014d
rilsonalmeida <rilson.almeida@gmail.com>
2019-05-09 11:49:50 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/pulsar/cursomc/services/CategoriaService.java
+++ src/main/java/com/pulsar/cursomc/services/CategoriaService.java
@@ -53,8 +53,8 @@ public class CategoriaService {
 		}	
 
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		@SuppressWarnings("deprecation")
-		PageRequest pageRequest = new PageRequest(page , linesPerPage, Direction.valueOf(direction), orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);		
 		return repo.findAll(pageRequest);
 	}
 	

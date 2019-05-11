eb392490d164166f835a5d3ee8fe83c52a40db15
rilsonalmeida <rilson.almeida@gmail.com>
2019-05-11 11:00:10 -0300

    Validacao customizada: email ´não repetido na inserção de Cliente

--- src/main/java/com/pulsar/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/pulsar/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pulsar.cursomc.domain.Cliente;
 import com.pulsar.cursomc.domain.dto.ClienteNewDTO;
 import com.pulsar.cursomc.domain.enums.TipoCliente;
+import com.pulsar.cursomc.repositories.ClienteRepository;
 import com.pulsar.cursomc.resources.exceptions.FieldMessage;
 import com.pulsar.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +37,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido !"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já está associado ao Cliente " + aux.getNome() + " - Id = " + aux.getId() + " !"));			
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();

1c214b78590423468f1982c0ccd5a120e912cad2
rilsonalmeida <rilson.almeida@gmail.com>
2019-05-10 23:03:46 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/pulsar/cursomc/resources/ClienteResource.java
+++ src/main/java/com/pulsar/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.pulsar.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pulsar.cursomc.domain.Cliente;
 import com.pulsar.cursomc.domain.dto.ClienteDTO;
+import com.pulsar.cursomc.domain.dto.ClienteNewDTO;
 import com.pulsar.cursomc.services.ClienteService;
 
 @RestController
@@ -34,9 +37,19 @@ public class ClienteResource {
 	}
 
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();		
+	}	
+	
+		
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
-		Cliente obj = service.fromDTO(objDTO);
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
--- src/main/java/com/pulsar/cursomc/services/ClienteService.java
+++ src/main/java/com/pulsar/cursomc/services/ClienteService.java
@@ -9,60 +9,105 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.pulsar.cursomc.domain.Cidade;
 import com.pulsar.cursomc.domain.Cliente;
+import com.pulsar.cursomc.domain.Endereco;
 import com.pulsar.cursomc.domain.dto.ClienteDTO;
+import com.pulsar.cursomc.domain.dto.ClienteNewDTO;
+import com.pulsar.cursomc.domain.enums.TipoCliente;
 import com.pulsar.cursomc.repositories.ClienteRepository;
+import com.pulsar.cursomc.repositories.EnderecoRepository;
 import com.pulsar.cursomc.services.exceptions.DataIntegrityException;
 import com.pulsar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
- 	@Autowired
+
+	@Autowired
 	private ClienteRepository repo;
-	
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
+
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		}	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
 
-	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
+
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
-	}	
-		
+	}
+
 	public void delete(Integer id) {
+
 		find(id);
-		
 		try {
-			repo.deleteById(id);			
-			}
-		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma entidade com outras associadas!");
+			repo.deleteById(id);
 		}
-	}		
-	
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+
+	}
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
-		}	
+	}
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
-				orderBy);		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
-	public Cliente fromDTO(ClienteDTO objDTO) { 
-		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
-	}	
-	
-	private void updateData(Cliente newObj, Cliente obj ) {
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+
+		cli.getEnderecos().add(end);
+
+		cli.getTelefones().add(objDto.getTelefone1());
+
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
+
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 }

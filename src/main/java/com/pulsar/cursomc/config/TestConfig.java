b32d954e03f52bbe52bd1d03944f68520723b923
rilsonalmeida <rilson.almeida@gmail.com>
2019-05-13 21:01:02 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/pulsar/cursomc/config/TestConfig.java
+++ src/main/java/com/pulsar/cursomc/config/TestConfig.java
@@ -8,19 +8,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.pulsar.cursomc.services.DBService;
+import com.pulsar.cursomc.services.EmailService;
+import com.pulsar.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
+
 	
 	@Autowired
 	private DBService dbService;
 	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
 
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+	
+	
 }
